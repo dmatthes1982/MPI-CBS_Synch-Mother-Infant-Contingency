d94a5d4389a076f11c3e03f7501c7e1c5243f85b
Daniel Matthes <dmatthes@cbs.mpg.de>
2018-10-02 14:44:32 +0200

    Function coSMIC_PSDoverDyads.m – important BUG fix → duplicates in the dyad selection vector will now be ignored to avoid false results → the variable numOfDyads is set to the actual selected number of dyads after user input → otherwise the pipeline crashes, when not all available dyads were selected. Function coSMIC_TFRoverDyads.m – important BUG fix → duplicates in the dyad selection vector will now be ignored to avoid false results → the variable numOfDyads is set to the actual selected number of dyads after user input → otherwise the pipeline crashes, when not all available dyads were selected. Function coSMIC_mPLVoverDyads.m – important BUG fix → duplicates in the dyad selection vector will now be ignored to avoid false results → numOfDyads will be set to length(listOfDyads) after user input → as a consequence the following length(listOfDyads) entries were replaced with the variable numOfDyads

--- functions/coSMIC_PSDoverDyads.m
+++ functions/coSMIC_PSDoverDyads.m
@@ -55,7 +55,8 @@ while selection == false
     cprintf([1,0.5,0], 'Wrong input!\n');
   else
     selection = true;
-    listOfDyads = x;
+    listOfDyads = unique(x);
+    numOfDyads  = length(listOfDyads);
   end
 end
 fprintf('\n');
