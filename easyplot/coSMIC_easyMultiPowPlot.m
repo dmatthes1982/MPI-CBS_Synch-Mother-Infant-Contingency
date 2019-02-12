952fd02d8d1daed93c816e3d75ec415746dd4b1f
Daniel Matthes <dmatthes@cbs.mpg.de>
2019-02-12 12:26:12 +0100

    Function coSMIC_easyMultiPowPlot – new config option cg.log → facilitates power graphs with a logarithmic scaling of the power Function coSMIC_easyPowPlot – new config option cg.log → facilitates power graphs with a logarithmic scaling of the power Function coSMIC_easyTopolot – new config option cfg.zlim → limits for color dimension → see all possible options in the reference documentation of the function

--- easyplot/coSMIC_easyMultiPowPlot.m
+++ easyplot/coSMIC_easyMultiPowPlot.m
@@ -13,6 +13,7 @@ function coSMIC_easyMultiPowPlot(cfg, data)
 %   cfg.baseline    = baseline condition (default: [], can by any valid condition)
 %                     the values of the baseline condition will be subtracted
 %                     from the values of the selected condition (cfg.condition)
+%   cfg.log         = use a logarithmic scale for the y axis, options: 'yes' or 'no' (default: 'no')
 %
 % This function requires the fieldtrip toolbox
 %
@@ -26,6 +27,7 @@ function coSMIC_easyMultiPowPlot(cfg, data)
 cfg.part      = ft_getopt(cfg, 'part', 'mother');
 cfg.condition = ft_getopt(cfg, 'condition', 11);
 cfg.baseline  = ft_getopt(cfg, 'baseline', []);
+cfg.log       = ft_getopt(cfg, 'log', 'no');
 
 if ~ismember(cfg.part, {'mother', 'child'})                                 % check cfg.part definition
   error('cfg.part has to either ''mother'' or ''child''.');
@@ -81,6 +83,10 @@ if isempty(cfg.baseline)                                                    % ex
 else
   datamatrix = squeeze(dataPlot.powspctrm(trialNum,selchan,:)) - ...        % subtract baseline condition
                 squeeze(dataPlot.powspctrm(baseNum,selchan,:));
+end
+
+if strcmp(cfg.log, 'yes')
+  datamatrix = 10 * log10( datamatrix );
 end
 
 xval        = dataPlot.freq;                                                % extract the freq vector
